0746b29571c204f97c30966361bdf905cc2e15d0
Hao Zhou <hzhou.me@gmail.com>
2018-03-31 20:15:41 -0400

    init

---
+++ app/src/main/java/me/hzhou/journaler/activity/MainActivity.kt
@@ -0,0 +1,15 @@
+package me.hzhou.journaler.activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import me.hzhou.journaler.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+}
