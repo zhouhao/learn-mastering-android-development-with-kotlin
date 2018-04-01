85e4cb5a3f33fdc1005d1406d03bed48f7d57b75
Hao Zhou <hzhou.me@gmail.com>
2018-03-31 21:03:05 -0400

    add placeholder for Note and Todo activities

---
+++ app/src/main/java/me/hzhou/journaler/activity/ItemActivity.kt
@@ -0,0 +1,7 @@
+package me.hzhou.journaler.activity
+
+/**
+ * Created by hzhou on 3/31/18.
+ * Email: hzhou.me@gmail.com
+ */
+abstract class ItemActivity : BaseActivity()
---
+++ app/src/main/java/me/hzhou/journaler/activity/NoteActivity.kt
@@ -0,0 +1,13 @@
+package me.hzhou.journaler.activity
+
+import me.hzhou.journaler.R
+
+/**
+ * Created by hzhou on 3/31/18.
+ * Email: hzhou.me@gmail.com
+ */
+class NoteActivity : ItemActivity() {
+    override val tag: String = javaClass.name
+    override fun getLayout() = R.layout.activity_note
+
+}
