c210013936189faff283ab082df643d1ab00b503
Hao Zhou <hzhou.me@gmail.com>
2018-04-02 00:10:34 -0400

    add event to floatingActionButton, and can start note/todo create activity

--- app/src/main/java/me/hzhou/journaler/activity/ItemActivity.kt
+++ app/src/main/java/me/hzhou/journaler/activity/ItemActivity.kt
@@ -1,6 +1,9 @@
 package me.hzhou.journaler.activity
 
+import android.os.Bundle
+import android.util.Log
 import me.hzhou.journaler.R
+import me.hzhou.journaler.model.MODE
 
 /**
  * Created by hzhou on 3/31/18.
@@ -8,4 +11,12 @@ import me.hzhou.journaler.R
  */
 abstract class ItemActivity : BaseActivity() {
     override fun getActivityTitle() = R.string.app_name
+    protected var mode = MODE.VIEW
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val modeToSet = intent.getIntExtra(MODE.EXTRAS_KEY, MODE.VIEW.mode)
+        mode = MODE.getByValue(modeToSet)
+        Log.v(tag, "Mode [ $mode ]")
+    }
 }
--- app/src/main/java/me/hzhou/journaler/fragment/ItemsFragment.kt
+++ app/src/main/java/me/hzhou/journaler/fragment/ItemsFragment.kt
@@ -1,6 +1,17 @@
 package me.hzhou.journaler.fragment
 
+import android.content.Intent
+import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
+import android.support.v7.app.AlertDialog
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import me.hzhou.journaler.R
+import me.hzhou.journaler.activity.NoteActivity
+import me.hzhou.journaler.activity.TodoActivity
+import me.hzhou.journaler.model.MODE
 
 /**
  * Created by hzhou on 4/1/18.
@@ -9,4 +20,37 @@ import me.hzhou.journaler.R
 class ItemsFragment : BaseFragment() {
     override val logTag: String = javaClass.name
     override fun getLayout() = R.layout.fragment_items
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+        val view = inflater.inflate(getLayout(), container, false)
+        val btn = view.findViewById<FloatingActionButton>(R.id.new_item)
+        btn.setOnClickListener {
+            val items = arrayOf(getString(R.string.todos), getString(R.string.notes))
+            val builder = AlertDialog.Builder(this@ItemsFragment.context!!)
+                    .setTitle(R.string.choose_a_type)
+                    .setItems(items, { _, which ->
+                        when (which) {
+                            0 -> openCreateTodo()
+                            1 -> openCreateNote()
+                            else -> Log.e(logTag, "Unknown option selected [ $which ]")
+                        }
+                    })
+            builder.show()
+        }
+        return view
+    }
+
+    private fun openCreateNote() {
+        val intent = Intent(context, NoteActivity::class.java)
+        intent.putExtra(MODE.EXTRAS_KEY, MODE.CREATE.mode)
+        startActivity(intent)
+    }
+
+    private fun openCreateTodo() {
+        val intent = Intent(context, TodoActivity::class.java)
+        intent.putExtra(MODE.EXTRAS_KEY, MODE.CREATE.mode)
+        startActivity(intent)
+
+    }
 }
