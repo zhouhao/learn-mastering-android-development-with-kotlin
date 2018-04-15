74c267178a4ac8dbd7d48a1477755dc6abba51d7
Hao Zhou <hzhou.me@gmail.com>
2018-04-15 18:52:18 -0400

    make Todo class Parcelable

--- app/src/main/java/me/hzhou/journaler/model/Todo.kt
+++ app/src/main/java/me/hzhou/journaler/model/Todo.kt
@@ -1,6 +1,8 @@
 package me.hzhou.journaler.model
 
 import android.location.Location
+import android.os.Parcel
+import android.os.Parcelable
 
 /**
  * Created by hzhou on 4/2/18.
@@ -9,7 +11,30 @@ import android.location.Location
 class Todo(title: String,
            message: String,
            location: Location,
-           var scheduledFor: Long) : Entry(title, message, location) {
-
+           var scheduledFor: Long) : Entry(title, message, location), Parcelable {
     override var id = 0L
+
+    constructor(source: Parcel) : this(
+            source.readString(),
+            source.readString(),
+            source.readParcelable<Location>(Location::class.java.classLoader),
+            source.readLong()
+    )
+
+    override fun describeContents() = 0
+
+    override fun writeToParcel(dest: Parcel, flags: Int) = with(dest) {
+        writeString(title)
+        writeString(message)
+        writeParcelable(location, 0)
+        writeLong(scheduledFor)
+    }
+
+    companion object {
+        @JvmField
+        val CREATOR: Parcelable.Creator<Todo> = object : Parcelable.Creator<Todo> {
+            override fun createFromParcel(source: Parcel): Todo = Todo(source)
+            override fun newArray(size: Int): Array<Todo?> = arrayOfNulls(size)
+        }
+    }
 }
